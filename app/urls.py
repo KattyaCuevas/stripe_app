fcedc2324fd0196bba07d6639e02dc3a1caa47d4
Kattya Cuevas <kattya.cuevas@gmail.com>
2016-12-19 19:15:52 -0500

    Change view methods to view classes

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
+from .views import LoginView, RegisterView
 
 app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'register', views.register, name='register'),
-    url(r'login', views.login, name='login'),
-    url(r'^(?P<user_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'register', RegisterView.as_view(), name='register'),
+    url(r'login', LoginView.as_view(), name='login'),
+    url(r'me', views.detail, name='detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -9,33 +9,36 @@ from .forms import RegisterUser, LoginUser
 def index(request):
     return render(request, 'user/index.html')
 
-def detail(request, user_id):
-    user = get_object_or_404(StripeUser, pk=user_id)
+def detail(request):
+    user = get_object_or_404(StripeUser, pk=request.session['user_id'])
     return render(request, 'user/detail.html', {'user': user})
 
-def register(request):
-    if request.method == 'POST':
+class RegisterView(View):
+    def post(self, request):
         form = RegisterUser(request.POST)
         if form.is_valid():
             new_user = form.save()
             request.session['user_id'] = new_user.pk
             new_user.set_password(request.POST['password'])
             new_user.save()
-            return redirect('app:detail', user_id=new_user.pk)
-    else:
+            return redirect('app:detail')
+        else:
+            return render(request, 'user/register.html', { 'form': form })
+
+    def get(self, request):
         form = RegisterUser()
+        return render(request, 'user/register.html', { 'form': form })
 
-    return render(request, 'user/register.html', { 'form': form })
-
-def login(request):
-    if request.method == 'POST':
+class LoginView(View):
+    def post(self, request):
         form = LoginUser(request.POST)
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
-            return redirect('app:detail', user_id=user.pk)
+            request.session['user_id'] = user.pk
+            return redirect('app:detail')
         else:
             return render(request, 'user/login.html', { 'form': form })
-    else:
-        form = LoginUser()
 
-    return render(request, 'user/login.html', { 'form': form })
+    def get(self, request):
+        form = LoginUser()
+        return render(request, 'user/login.html', { 'form': form })
